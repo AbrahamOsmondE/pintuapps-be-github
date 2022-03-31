7b08e763b57ccc3cb148e810e099a930af5c9101
AbrahamOsmondE <abraham.osmond@gmail.com>
2022-04-01 02:17:57 +0800

    put shop create update delete

--- shop/serializers.py
+++ shop/serializers.py
@@ -71,36 +71,52 @@ class ShopSerializer(ModelSerializer):
         data = self.context["request"]
         newCustomFields = data.pop("custom_fields")
         newShopItems = data.pop("shop_items")
-        # customFields = list(ShopCustom.objects.filter(shop_id=instance))
-        # shopItems = list(ShopItem.objects.filter(shop_id=instance))
+        customFields = list(ShopCustom.objects.filter(shop_id=instance))
+        shopItems = list(ShopItem.objects.filter(shop_id=instance))
+        existing_shop_customs = set()
+        existing_shop_items = set()
 
-        # Delete the ShopCustom objects
-        # for shopcustom in customFields:
-        #     shopcustom.delete()
+        for shopcustom in customFields:
+            existing_shop_customs.add(int(shopcustom.id))
 
-        # Create new ShopCustom objects
         for shopcustom in newCustomFields:
-            custom_fields = ShopCustom.objects.get(id=shopcustom["id"])
-            custom_fields.type = shopcustom["type"]
-            custom_fields.placeholder = shopcustom["placeholder"]
-            custom_fields.options = shopcustom["options"]
-            custom_fields.save()
+            if shopcustom.id in existing_shop_customs:
+                custom_fields = ShopCustom.objects.get(id=shopcustom["id"])
+                custom_fields.type = shopcustom["type"]
+                custom_fields.placeholder = shopcustom["placeholder"]
+                custom_fields.options = shopcustom["options"]
+                custom_fields.save()
+                existing_shop_customs.remove(shopcustom.id)
+            else:
+                custom_fields = ShopCustom(shop_id=instance, type=shopcustom["type"], placeholder=shopcustom["placeholder"], options=shopcustom["options"])
+                custom_fields.save()
 
-        # Delete the ShopItem objects
-        # for shopitems in shopItems:
-        #     shopitems.delete()
+        for shopitems in shopItems:
+            existing_shop_items.add(int(shopitems.id))
 
-        # Create new ShopItem objects
         for shopitems in newShopItems:
-            shop_item = ShopItem.objects.get(id=shopitems["id"])
-            shop_item.item_name = shopitems["item_name"]
-            shop_item.description=shopitems["description"]
-            shop_item.price=shopitems["price"]
-            shop_item.original_quantity=shopitems["original_quantity"]
-            shop_item.display_picture=shopitems.get("display_picture", "")
+            if shopitems.id in existing_shop_items:
+                shop_item = ShopItem.objects.get(id=shopitems["id"])
+                shop_item.item_name = shopitems["item_name"]
+                shop_item.description=shopitems["description"]
+                shop_item.price=shopitems["price"]
+                shop_item.original_quantity=shopitems["original_quantity"]
+                shop_item.display_picture=shopitems.get("display_picture", "")
+                shop_item.save()
+                existing_shop_items.remove(shopitems.id)
+            else:
+                shop_item = ShopItem(shop_id=instance, item_name=shopitems["item_name"], description=shopitems["description"],
+                                  price=shopitems["price"], original_quantity=shopitems["original_quantity"], display_picture=shopitems.get("display_picture", ""))
+                shop_item.save()
 
-            shop_item.save()
-
+        for shop_custom_id in existing_shop_customs:
+            shop_custom = ShopCustom.objects.get(id=shop_custom_id)
+            shop_custom.delete()
+        
+        for shop_item_id in existing_shop_items:
+            shop_item = ShopItem.objects.get(id=shop_item_id)
+            shop_item.delete()
+            
         # Update the instance which is the Shop with the data
         instance.shop_owner = data.get(
             "shop_owner", instance.shop_owner)
