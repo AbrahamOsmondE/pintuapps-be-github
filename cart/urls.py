31e774805f52a2d642291176c1adcd6a7f49808a
AbrahamOsmondE <abraham.osmond@gmail.com>
2022-03-31 21:32:58 +0800

    backslash

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('cart_items/',views.CartItemsAPI.as_view()),
-    path('cart_items/<int:cart_item_id>',views.SingleCartItemsAPI.as_view()),
-    path('shop_items/<int:shop_id>',views.ShopItemsAPI.as_view()),
-    path('shop_item/<int:shop_item_id>',views.SingleShopItemsAPI.as_view())
+    path('cart_items/<int:cart_item_id>/',views.SingleCartItemsAPI.as_view()),
+    path('shop_items/<int:shop_id>/',views.ShopItemsAPI.as_view()),
+    path('shop_item/<int:shop_item_id>/',views.SingleShopItemsAPI.as_view())
 ]
--- order/urls.py
+++ order/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("order/seller/detailed/<str:shop_id>/",
          views.SellerDetailedShopOrder.as_view(), name="SellerDetailedShopOrder"),
     path("order/", views.OrderList.as_view(), name="OrderList"),
-    path("order/payment",views.OrderPayment.as_view(),name="OrderPayment"),
-    path("order/status",views.OrderPaymentStatus.as_view(),name="OrderPaymentStatus"),
+    path("order/payment/",views.OrderPayment.as_view(),name="OrderPayment"),
+    path("order/status/",views.OrderPaymentStatus.as_view(),name="OrderPaymentStatus"),
     path("order/excel/<str:shop_id>/",views.OrderPaymentExcel.as_view(),name="OrderPaymentExcel")
 ]
