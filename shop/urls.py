c3f0f62efad71705f91a2548db374b485c8c23f7
AbrahamOsmondE <abraham.osmond@gmail.com>
2022-06-11 23:08:26 +0800

    update

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path("shops/", views.ShopsList.as_view(), name="shops"),
-    # path("shops/<str:shop_id>/", views.ShopDetails.as_view(), name="shop"),
-    # path("shops/<str:shop_id>/<str:shop_item_id>/", views.ShopItemDetails.as_view(), name="item"),
-    # path("shop_item/<str:shop_item_id>/",views.ShopItemAPI.as_view(),name="ShopItemAPI")
+    path("shops/", views.ShopsList.as_view(), name="shops"),
+    path("shops/<str:shop_id>/", views.ShopDetails.as_view(), name="shop"),
+    path("shops/<str:shop_id>/<str:shop_item_id>/", views.ShopItemDetails.as_view(), name="item"),
+    path("shop_item/<str:shop_item_id>/",views.ShopItemAPI.as_view(),name="ShopItemAPI")
 ]
